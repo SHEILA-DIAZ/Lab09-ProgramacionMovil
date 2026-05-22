68fef75402033e6579fe9b1c7aeeca5d89b555c2
Sheila Diaz Rojas <sheila.diaz@tecsup.edu.pe>
2026-05-22 17:15:44 -0500

    feat: Agregado PostModel y PostEntity

---
+++ app/src/main/java/com/example/lab09/data/local/PostEntity.kt
@@ -0,0 +1,12 @@
+package com.example.lab09.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favorite_posts")
+data class PostEntity(
+    @PrimaryKey val id: Int,
+    val userId: Int,
+    val title: String,
+    val body: String
+)
